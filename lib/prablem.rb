3ae65ff47a3b9cd488dcc87ca9dc045c133311f9
Alexei Emam <alexei@chaptermedia.com>
2017-01-11 01:13:59 +0000

    Dump a rough object outline in

--- lib/prablem.rb
+++ lib/prablem.rb
@@ -6,8 +6,6 @@ require "prablem/version"
 module Prablem
   class Cmd
     include Commander::Methods
-    
-
     # Set the dir of the repo we care about
     working_dir = "/home/ubuntor/code/..." 
     g = Git.open(working_dir)#, :log => Logger.new(STDOUT))
@@ -26,3 +24,51 @@ module Prablem
 
   end
 end
+
+# id: # AR-Migration style timestamp+CamelCaseShortDescription
+
+# antecedent: # i.e. ticket source / github issue parent / slack message URL # some reference that hints at the reasoning behind the necessity of this
+
+# product_base_version: # branch+commit-ish sha(+appversion?) at time of definition
+
+# description: # github issue url # evidence of the existence of the problem required at the referenced location
+# resolution: # github PR url # evidence of resolution required at the referenced location
+
+# ## To appear programmatically in automated release notices / changelogs ## maybe required on resolve?
+# human_description: 'Short sentence to describe the Product's problem'
+# human_resolution: 'Short sentence to describe the effects of the resolution on the product (only include implementation details if exceptionally exceptional)'
+
+# dependencies: [] # issues spawned directly from this work whose resolution is required to satisfy the spirit+letter of the problem # it is valid (and encouraged especially in the case of exploratory work) for a resolution to consist entirely of defining a collection of new (hopefully more specific/informed) problems that require resolution.
+
+# resolution_notes: [] # e.g. formalised list of resolution-specific implementation instructions COMPARED TO PRODUCT_BASE_VERSION (e.g. migrations, external service configurations, external application upgrades) so release complexity of work can be qualified/quantified. 
+
+# defined_at: 
+# defined_by:
+
+# resolved_at:
+# resolved_by:
+
+# ### UNDECIDED ###
+# productsignoff: [time + link to evidence (usually an absolute reference to a specific element in the resolution PR, but could be external CI success link)]
+# peerssatisfied: [time + link to peer signoff (usually as PR comments)]
+# productstakeholdersatisfied [timestamp + link to stakeholder signoff (usually in external ticketing system)]
+# ### ###
+
+# #### UNDECIDED WHETHER TO ALLOW/ENFORCE INCREMENTAL LOGS ####
+# #### IN PRINCIPLE THE SEMI-ARBITRARY 4 HOUR WORKTIME MAX PER BLOB SHOULD MAKE THIS REDUNDANT ####
+# #### IN PRACTICE, EVERYTHING IS GROSS AND TERRIBLE ####
+# time_invested_logs: [
+#   timestamp: duration,
+#   timestamp: duration,
+#   ...
+#   timestamp: duration
+# ]
+
+# time_invested_override: duration
+
+
+# commands
+
+# prablem define
+# prablem resolve
+# prablem log
